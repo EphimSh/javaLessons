3db066432e05a859759a6dc363eecf0078137717
Ephimiz <37134016+Ephimiz@users.noreply.github.com>
2022-09-06 22:57:43 +0900

    homework2
    here is my solution

--- HomeWorkApp.java
+++ HomeWorkApp.java
@@ -1,60 +1,70 @@
-package lesson1.homework;
-import java.lang.Math;
-public class HomeWorkApp {
+package lesson2.homework2;
+
+
+public class homework2 {
+
     public static void main(String[] args) {
-        printThreeWords();
-        System.out.println("**************");
-        checkSumSign();
-        System.out.println("**************");
-        printColor();
-        System.out.println("**************");
-        compareNumbers();
+        checkSumIsBetweenTenAndTwenty(16, 5);
+        checkSumIsPositive((int) ((Math.random() * 10) - 5));
+        checkSumIsNegative((int) ((Math.random() * 10) - 5));
+        printSomeTextAmountOfTimes("ayo", 10);
+        leapYearCheck(401); //  TEST DATA: 3 - false, 4 - true, 100 - false, 104 - true,200 - false, 400 - true, 600 - false, 800 - true;
     }
 
+    private static int checkSumIsBetweenTenAndTwenty(int a, int b) {
+        int check = a + b;
 
+        if (check >= 10 && check <= 20) {
+            System.out.println("Sum is in the range");
 
-    public static void printThreeWords(){
-        System.out.println("Orange");
-        System.out.println("Banana");
-        System.out.println("Apple");
-    }
-    public static void  checkSumSign(){
-        int a = (int) ((Math.random() - 0.3) * 10);
-        int b = (int) ((Math.random() - 0.1) * 10);
-
-        if ((a + b) >= 0) {
-            System.out.println("a: " + a);
-            System.out.println("b: " + b);
-            System.out.println("a + b = " + (a+b) + " Сумма положительная");
-        } else if ((a + b) < 0) {
-            System.out.println("a: " + a);
-            System.out.println("b: " + b);
-            System.out.println("a + b = " + (a+b) + " Сумма отрицательная");
-        }
-    }
-    public static void printColor(){
-        int value =  (int) ((Math.random() - 0.2) * 150);
-
-        if(value <= 0) {
-            System.out.println( value + " Красный");
-        } else if (value > 0 &&  value <= 100) {
-            System.out.println( value + " Желтый");
-        } else if (value > 100 ){
-            System.out.println( value + " Зеленый");
-        }
-    }
-    public static void compareNumbers(){
-        int a = (int) (Math.random() * 10);
-        int b = (int) (Math.random() * 10);
-        if(a>=b){
-            System.out.println("a = "+ a);
-            System.out.println("b = "+ b);
-            System.out.println("a >= b");
         } else {
-            System.out.println("a = "+ a);
-            System.out.println("b = "+ b);
-            System.out.println("a < b");
+            System.out.println("sum is out of range");
+        }
+        return check;
+    }
+
+    private static int checkSumIsPositive(int a) {
+        if (a >= 0) {
+            System.out.println("Number is positive: " + a);
+        } else {
+            System.out.println("Number is negative: " + a);
+        }
+        return a;
+    }
+
+    private static boolean checkSumIsNegative(int a) {
+        return a < 0;
+    }
+
+    private static void printSomeTextAmountOfTimes(String a, int b) {
+        for (int i = 0; i < b; i++) {
+            System.out.println(a + ": " + (i + 1));
         }
     }
 
+    private static boolean leapYearCheck(int year) {
+
+         /* if(year % 4 != 0){
+            System.out.println("FALSE");
+            return false;
+        } else if (year % 100 != 0 && year % 400 != 0) {
+            System.out.println("FALSE");
+            return false;
+        } else {
+            System.out.println("TRUE");
+        }*/
+
+        /*if (year % 4 != 0 || year % 100 != 0 && year % 400 != 0){
+            System.out.println("FALSE");
+            return false;
+        } else {
+            System.out.println("TRUE");
+        }
+        return true;
+    }*/
+/*       boolean isLeapYear = (year % 4 == 0 || year % 400 == 0) || year % 100 == 0;
+        System.out.println(isLeapYear);
+        return isLeapYear;*/
+        return (year % 4 == 0 || year % 400 == 0) || year % 100 == 0;
+    }
 }
